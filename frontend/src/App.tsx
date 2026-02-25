924d2e4c89fd4994b78820309f1f0bcaaa4253f2
mani_kumar_avniya <manikumar.k@avniya.in>
2026-02-25 12:56:17 +0530

    added admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter future={{ v7_relativeSplatPath: true }}>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
